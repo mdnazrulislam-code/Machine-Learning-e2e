3cb80f7937a93d6b93434b946c92984412349892
Md Nazrul Islam <arfanislamabir@gmail.com>
2024-03-24 05:52:13 +0600

    data transformation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 
@@ -22,6 +23,18 @@ if __name__ == '__main__':
     try:
         logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<")
         obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<<<\n\nx========================x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+STAGE_NAME = "Data Transformation Stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<")
+        obj = DataTransformationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<<<\n\nx========================x")
     except Exception as e:
