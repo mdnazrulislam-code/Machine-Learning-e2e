0000cb559cbde557adfd1d771b950d8d9a7a0839
Md Nazrul Islam <arfanislamabir@gmail.com>
2024-03-24 14:45:15 +0600

    model trainer added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-
+from mlProject.pipeline.stage_04_model_trainer import ModelTrainingPipeline
 
 
 
@@ -35,6 +35,17 @@ if __name__ == '__main__':
     try:
         logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<")
         obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<<<\n\nx========================x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+STAGE_NAME = "Model Trainer "
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<")
+        obj = ModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<<<\n\nx========================x")
     except Exception as e:
---
+++ src/mlProject/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,31 @@
+from mlProject.config.configuration import ConfigurationManager
+from mlProject.components.model_trainer import ModelTrainer
+from mlProject import logger
+from pathlib import Path
+
+
+
+STAGE_NAME = "Model Trainer "
+
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<")
+        obj = ModelTrainer()
+        obj.main()
+        logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<<<\n\nx========================x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
